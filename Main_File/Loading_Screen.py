1850ad7e22192f2a7ca60a78e4ee89c0a36ead98
Virati Akira Nandhan Reddy <viratiaki53@gmail.com>
2025-01-08 22:58:07 +0530

    dev

--- Main_File/Loading_Screen.py
+++ Main_File/Loading_Screen.py
@@ -5,20 +5,6 @@ from time import sleep
 def Buffer_At_Activation(func):
     
     def Loading():
-        '''Load = tk.Tk()
-        Load.title('Loading')
-        # Load.resizable(False,False)
-      
-
-        Photo = tk.PhotoImage(file=fr'{Path}\Visual Data\Loading.gif',format=f'gif -index {frame}')
-        tk.Label(Load,image=Photo).pack(side='top')
-        sleep(0.1)
-        # tk.Label.config(Load,image=Photo)
-        # tk.Label.image = Photo
-
-        Load.after(delay,Loading(frame+1,delay))
-        Load.after(5000,Load.destroy)
-        Load.mainloop()'''
         
         func()
         
